1e364d71294fa7e2abaf9f438bfa244416bb95c1
ajh2oman <ajh2oman@gmail.com>
2023-03-01 12:36:22 -0500

    Update

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd c:\Users\A.J\Documents\GitHub\skiconditions
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
-git push origin master
